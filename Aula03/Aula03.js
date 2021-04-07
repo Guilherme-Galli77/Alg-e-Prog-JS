f8d82da9d43437366f77a5005d48d83dcd59adc2
Guilherme-Galli77 <79452612+Guilherme-Galli77@users.noreply.github.com>
2021-04-07 20:45:06 -0300

    Atualização

---
+++ Aula03/Aula03.js
@@ -0,0 +1,17 @@
+//Codigo da aula 2//
+
+alert("Codigo aula 2");
+
+//declaracao das variaveis
+var idade;
+var nomeDoAluno;
+
+//atribuicao de valor as variaveis
+idade = 10;
+nomeDoAluno = "Jose"; //String colocada entre aspas
+
+//atribuicao de uma constante que nao pode ser modificada 
+
+const PI = 3.14159265;
+
+alert("O numero de PI eh:",PI);
