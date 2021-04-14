1c45b2a26e5cd0d918b2190360b8c16f443b5aee
Guilherme-Galli77 <79452612+Guilherme-Galli77@users.noreply.github.com>
2021-04-14 01:37:32 -0300

    Codigos feitos pelo professor

---
+++ Aula04/Aula04-Teste dos numeros.js
@@ -0,0 +1,13 @@
+var numero;
+
+numero = prompt ("Insira um numero");
+
+if (numero > 0) {
+  alert("Esse numero eh positivo");
+}else{
+  if(numero < 0) {
+    alert("Esse numero eh negativo");
+  } else {
+    alert ("Esse numero eh neutro");
+  }
+}
