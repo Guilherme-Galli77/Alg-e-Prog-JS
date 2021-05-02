57f5d3ede1aa6a2d21bfe3dae5836f9cc1fa0942
Guilherme-Galli77 <79452612+Guilherme-Galli77@users.noreply.github.com>
2021-05-02 20:53:47 -0300

    Criação do laço de repetição do sistema
    Adição do sistema, ainda faltam adições estéticas

--- ProjetoPI/ProjetoPI.js
+++ ProjetoPI/ProjetoPI.js
@@ -7,20 +7,29 @@ var valorAlcool;
 var valorGasolina;
 var razaoBase;
 var divAlcoolGasolina
+var sistema = 'S'
 
 //atribuicao e calculo de valor das variaveis
+while(sistema == 'S'){
 razaoBase = 0.7;
 valorAlcool = prompt("Qual o preço do alcool: ");
 valorGasolina = prompt("Qual o preço da gasolina: ");
 divAlcoolGasolina = (valorAlcool/valorGasolina)
 
 
-if (divAlcoolGasolina > razaoBase)
-   alert("Vale a pena a gasolina!");
-else if (divAlcoolGasolina < razaoBase)
-   alert("Vale a pena o alcool!");
-else if (divAlcoolGasolina == razaoBase)
-   alert("Tanto faz um ou outro!");
-else
-   alert("ERRO NO SISTEMA!");
+if (divAlcoolGasolina > razaoBase){
+   alert("Vale a pena a gasolina!")}
+else if (divAlcoolGasolina < razaoBase){
+   alert("Vale a pena o alcool!")}
+else if (divAlcoolGasolina == razaoBase){
+   alert("Tanto faz um ou outro!")}
+else{
+   alert("ERRO NO SISTEMA!")
+}
 
+alert("Deseja inserir outros valores?");
+sistema = prompt("digite S para Sim e N para não")
+
+}
+
+alert("Obrigado por usar o comparador de preços!!!!")
